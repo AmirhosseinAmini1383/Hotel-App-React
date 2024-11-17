94c24a61340ec04f895acbc704ddac8a42a2bf0f
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-11-17 14:55:36 +0330

    Current Bookmark

--- src/components/SingleBookmark/SingleBookmark.jsx
+++ src/components/SingleBookmark/SingleBookmark.jsx
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useBookmark } from "../context/BookmarkListContext";
 import { useEffect } from "react";
 import Loader from "../Loader/Loader";
+import ReactCountryFlag from "react-country-flag";
 
 function SingleBookmark() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { getSingleBookmarks, currentBookmark, isLoadingCurrentBookmark } =
     useBookmark();
@@ -11,7 +13,21 @@ function SingleBookmark() {
     getSingleBookmarks(id);
   }, [id]);
   if (isLoadingCurrentBookmark || !currentBookmark) return <Loader />;
-  return <div>SingleBookmark</div>;
+  return (
+    <div>
+      <button onClick={() => navigate(-1)} className="btn btn--back">
+        &larr; Back
+      </button>
+      <h2 style={{ marginTop: "20px", marginBottom: "15px" }}>
+        {currentBookmark.cityName}
+      </h2>
+      <div className="bookmarkItem">
+        <ReactCountryFlag svg countryCode={currentBookmark.countryCode} />
+        &nbsp; <strong>{currentBookmark.cityName}</strong> &nbsp;
+        <span>{currentBookmark.country}</span>
+      </div>
+    </div>
+  );
 }
 
 export default SingleBookmark;
